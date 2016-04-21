2fe3d147f7e610e751c7c77741462f7f169db1dc
ncla <nucleaar@gmail.com>
2016-04-21 15:20:30 +0300

    Better hiding stuff without elements showing for few miliseconds, updated blocked ad list

--- contentscript.js
+++ contentscript.js
@@ -1,5 +1,4 @@
 function inject() {
-
     function urlDomain(data) {
         var a = document.createElement('a');
         a.href = data;
@@ -13,33 +12,32 @@ function inject() {
             }
         };
     }(window.open);
-
 }
 
-function addScript(template) {
-    var s = document.createElement("script");
-    if (template.src) {
-        s.src = template.src;
-    }
-    if (template.textContent) {
-        s.textContent = template.textContent;
-    }
-    document.documentElement.appendChild(s);
-}
+var scriptTag = document.createElement("script");
+scriptTag.textContent = inject.toString() + " inject()";
+document.documentElement.appendChild(scriptTag);
 
-addScript({
-    textContent: inject.toString() + " inject()"
-});
+var style =
+    'html, body { background: white !important; }' +
+    '#firstCollumn .boxNoDrag:first-child { display: none; }' +
+    '.topHolder a img { display: none; }' +
+    '#razer { display: none; }' +
+    'iframe[src*="wombo.gg"] { display: none; }' +
+    'a[href*="skinsanity.gg"] { display: none; }' +
+    'a[href*="egbaffiliates.com"] { display: none; }';
+
+var styleTag = document.createElement("style");
+styleTag.textContent = style;
+document.documentElement.appendChild(styleTag);
 
 $(document).ready(function() {
-    $('body, html').attr('style', 'background-image: none;');
-
     $('.topHolder a').each(function(i, v) {
         if(v.hostname !== 'hltv.org' && v.hostname !== 'www.hltv.org') {
             $(v).remove();
         }
     });
 
-    $('#firstCollumn').find('.boxNoDrag:eq(0)').remove();
-    $('a[href*="alphadraft.go2cloud.org"]').remove();
+    $('img[src*="txt_sponsors.gif"]').parents('li').hide();
+    $('a[href*="alphadraft.go2cloud.org"], iframe[src*="wombo.gg"]').remove();
 });
